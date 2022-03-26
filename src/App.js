eca5efe9fcb093ed7e956f5828d598ee7a73ee5e
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-03-26 14:09:02 +0500

    COnfigured the app ~ Bootstrap and Reactstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
