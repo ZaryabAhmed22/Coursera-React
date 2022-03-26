5f834513c3f1ed5214bc6014c4f6508a650af4e3
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-03-26 17:52:58 +0500

    Rednered Menu Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 
 import "./App.css";
+import Menu from "./components/Menu";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
