dd63fe6a04c53a83bb10f15977f6dcffbc314ff2
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-04-03 16:00:04 +0500

    Added homePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,46 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardText,
+  CardSubtitle,
+  CardTitle,
+} from "reactstrap";
 
-export default function () {
+function RenderCard({ item }) {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : (
+          ""
+        )}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default function Home(props) {
   return (
     <div className="container">
-      <h4>Home</h4>
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dish} />
+        </div>
+
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotion} />
+        </div>
+
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leader} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,8 @@
 import { Component } from "react";
-import { DISHES } from "../shared/data";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import DishDetail from "./DishDetail";
@@ -15,6 +18,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
       selectedDishId: null,
     };
   }
@@ -26,11 +32,22 @@ class Main extends Component {
   }
 
   render() {
+    //Home component function to pass in props
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
-          <Route path="/home" component={Home} />
+          <Route path="/home" component={HomePage} />
           <Route
             exact
             path={"/menu"}
